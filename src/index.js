8fc2ca4a2f591783df6da4b223218a9902656103
= <=>
2022-04-01 03:08:51 -0600

    Fixing react router error on importing package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import * as ReactDOMClient from "react-dom/client";
-import { Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
 ReactDOMClient.createRoot(document.getElementById("root")).render(
