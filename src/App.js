bf626fabe112e03f26ce8d6fe8e0e454e8cb5a59
= <=>
2022-04-01 02:45:02 -0600

    Setting up react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { Routes } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
-  return <main></main>;
+  return (
+    <main>
+      <Routes></Routes>
+    </main>
+  );
 }
 
 export default App;
