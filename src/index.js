3667f6b7bc66cc162387684044900830eca326b6
= <=>
2022-04-01 03:06:46 -0600

    Changin to new React 18 API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { HashRouter as Router } from "react-router-dom";
+import * as ReactDOMClient from "react-dom/client";
+import { Router } from "react-router-dom";
 import App from "./App";
 
-ReactDOM.render(
+ReactDOMClient.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
