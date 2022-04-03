778f77313a771a8cb62c9b9d3308467028f70b4b
= <=>
2022-04-03 05:13:05 -0600

    Adding header en food cards to index

--- src/components/Index/index.js
+++ src/components/Index/index.js
@@ -5,19 +5,32 @@ import Header from "../Header";
 import getProducts from "../../services/getProducts";
 
 import "./index.scss";
+import FoodCard from "../FoodCard";
 
 export default function Index() {
-  const [products, setProducts] = useState(null);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     getProducts().then((res) => {
-      setProducts(res);
+      setProducts(res.data);
     });
   }, []);
 
   return (
-    <div>
+    <div className="vending-machine">
       <Header />
+      <h1 className="main-title">Bienvenido usuario!</h1>
+      <div className="cards">
+        {products.map((food, i) => (
+          <FoodCard
+            key={i}
+            id={food.id}
+            name={food.name}
+            preparation_time={food.preparation_time}
+            thumbnail={food.thumbnail}
+          />
+        ))}
+      </div>
     </div>
   );
 }
