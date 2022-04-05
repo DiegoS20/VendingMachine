3156d7a38d30e2799cc9fe058aca2fc59117a8c0
Diego Saravia <saravia__2000@hotmail.com>
2022-04-05 04:12:31 -0600

    Adding flag to show or hide component

--- src/components/LateralMenuItemsSelected/index.js
+++ src/components/LateralMenuItemsSelected/index.js
@@ -5,11 +5,14 @@ import "./index.scss";
 
 import FoodContext from "../Index/FoodContext";
 
-export default function LateralMenuItemsSelected() {
+export default function LateralMenuItemsSelected({ disable }) {
   const { foodQueue } = useContext(FoodContext);
 
   return (
-    <div className="lateral-menu-items-selected">
+    <div
+      className="lateral-menu-items-selected"
+      style={{ display: disable ? "none" : "unset" }}
+    >
       {foodQueue.map((item, i) => (
         <ItemsSelectedItem key={i} item={item} />
       ))}
