e3e3b6822d7f2062509aa406ee370bc654f62d63
Diego Saravia <saravia__2000@hotmail.com>
2022-04-05 04:33:12 -0600

    Adding functionality to the menu

--- src/components/LateralMenu/index.js
+++ src/components/LateralMenu/index.js
@@ -1,36 +1,45 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import LateralMenuItemsSelected from "../LateralMenuItemsSelected";
 import LateralMenuItemsDispatched from "../LateralMenuItemsDispatched";
 
 import "./index.scss";
+import menuIcon from "./images/menu.png";
 
 export default function LateralMenu() {
   const menuTabs = ["Items selected", "Items dispatched"];
   const [selectedTab, setSelectedTab] = useState(0);
+  const menuRef = useRef();
 
-  function handleCloseBtnClick() {}
+  function toggleMenu() {
+    menuRef.current.classList.toggle("active");
+  }
 
   return (
-    <div className="lateral-menu">
-      <div className="close">
-        <span className="close-x" onClick={handleCloseBtnClick}>
-          X
-        </span>
+    <div className="lateral-menu active" ref={menuRef}>
+      <div className="open-icon" onClick={toggleMenu}>
+        <img src={menuIcon} alt="Menu icon" />
       </div>
-      <div className="items">
-        {menuTabs.map((tab, i) => (
-          <div
-            key={i}
-            className={`menu-tab${selectedTab === i ? " selected" : ""}`}
-            onClick={() => setSelectedTab(i)}
-          >
-            <div className="menu-tab-title">{tab}</div>
-          </div>
-        ))}
-      </div>
-      <div className="content">
-        <LateralMenuItemsSelected disable={selectedTab === 1} />
-        <LateralMenuItemsDispatched disable={selectedTab === 0} />
+      <div className="menu">
+        <div className="close">
+          <span className="close-x" onClick={toggleMenu}>
+            X
+          </span>
+        </div>
+        <div className="items">
+          {menuTabs.map((tab, i) => (
+            <div
+              key={i}
+              className={`menu-tab${selectedTab === i ? " selected" : ""}`}
+              onClick={() => setSelectedTab(i)}
+            >
+              <div className="menu-tab-title">{tab}</div>
+            </div>
+          ))}
+        </div>
+        <div className="content">
+          <LateralMenuItemsSelected disable={selectedTab === 1} />
+          <LateralMenuItemsDispatched disable={selectedTab === 0} />
+        </div>
       </div>
     </div>
   );
