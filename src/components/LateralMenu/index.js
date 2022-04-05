6e4ae24a48e887d7293ba5a4b9fb771ebb2ad9b3
Diego Saravia <saravia__2000@hotmail.com>
2022-04-05 04:11:52 -0600

    Changin way to render both component

--- src/components/LateralMenu/index.js
+++ src/components/LateralMenu/index.js
@@ -1,13 +1,11 @@
 import { useState } from "react";
 import LateralMenuItemsSelected from "../LateralMenuItemsSelected";
+import LateralMenuItemsDispatched from "../LateralMenuItemsDispatched";
 
 import "./index.scss";
 
 export default function LateralMenu() {
-  const menuTabs = [
-    ["Items selected", <LateralMenuItemsSelected />],
-    ["Items dispatched", <LateralMenuItemsSelected />],
-  ];
+  const menuTabs = ["Items selected", "Items dispatched"];
   const [selectedTab, setSelectedTab] = useState(0);
 
   function handleCloseBtnClick() {}
@@ -26,11 +24,14 @@ export default function LateralMenu() {
             className={`menu-tab${selectedTab === i ? " selected" : ""}`}
             onClick={() => setSelectedTab(i)}
           >
-            <div className="menu-tab-title">{tab[0]}</div>
+            <div className="menu-tab-title">{tab}</div>
           </div>
         ))}
       </div>
-      <div className="content">{menuTabs[selectedTab][1]}</div>
+      <div className="content">
+        <LateralMenuItemsSelected disable={selectedTab === 1} />
+        <LateralMenuItemsDispatched disable={selectedTab === 0} />
+      </div>
     </div>
   );
 }
